32bba07c77b21624dc8aa9068e3880cbdd8ceaf4
mwanikukala-rgb <mwanikukala@gmail.com>
2026-08-24 16:06:01 +0200

    Simplify root element rendering in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/global.css";
 
-const rootElement = document.getElementById("root");
-
-if (!rootElement) {
-  throw new Error("BembaTranslate: root element was not found.");
-}
-
-ReactDOM.createRoot(rootElement).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
