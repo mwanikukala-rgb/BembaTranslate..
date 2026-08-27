a46455fac4886a3be6c0bee2031c6df7d7978b2b
mwanikukala-rgb <mwanikukala@gmail.com>
2026-08-27 13:52:32 +0200

    Update bembaDictionary.ts

--- src/data/bembaDictionary.ts
+++ src/data/bembaDictionary.ts
@@ -10,44 +10,7 @@ export const bembaDictionary: BembaDictionaryEntry[] = [
   { english: "know", bemba: "Ukwishiba" },
 
   { english: "to think", bemba: "Ukutontonkanya" },
-  { english: "think", bemba: "Ukutontonkanya" },
-
-  { english: "messenger", bemba: "Kapaaso" },
-  { english: "volunteer", bemba: "Kaipela" },
-  { english: "chief", bemba: "Imfumu" },
-  { english: "gift", bemba: "Ubupe" },
-  { english: "village", bemba: "Umushi" },
-  { english: "health", bemba: "Ubuumi" },
-  { english: "president", bemba: "Kateka" },
-  { english: "headman", bemba: "Mwine mushi" },
-  { english: "farming", bemba: "Ubulimi" },
-  { english: "people", bemba: "Abantu" },
-  { english: "money", bemba: "Indalama" },
-  { english: "development", bemba: "Ubuyantanshi" },
-  { english: "languages", bemba: "Indimi" },
-
-  { english: "without problems", bemba: "Ukwabula amafya" },
-  { english: "to speak the languages", bemba: "Ukulanda indimi" },
-  { english: "to bring money", bemba: "Ukuleta Indalama" },
-
-  { english: "to see", bemba: "Ukumona" },
-  { english: "to see each other", bemba: "Ukumonana" },
-  { english: "to want", bemba: "Ukufwaya" },
-  { english: "to come from", bemba: "Ukufuma" },
-  { english: "to come", bemba: "Ukwisa" },
-  { english: "to talk", bemba: "Ukulanda" },
-  { english: "to tell", bemba: "Ukweba" },
-  { english: "to learn", bemba: "Ukusambilila" },
-  { english: "to wait", bemba: "Ukupembela" },
-  { english: "to enter", bemba: "Ukwingila" },
-  { english: "to lack something", bemba: "Ukubula" },
-  { english: "to volunteer", bemba: "Ukuipeela" },
-  { english: "to cultivate", bemba: "Ukulima" },
-  { english: "to allow", bemba: "Ukusuminisha" },
-  { english: "to accept", bemba: "Ukusuminisha" },
-  { english: "to agree", bemba: "Ukusuminisha" },
-  { english: "to help", bemba: "Ukwafwa" },
-
+  
   {
     english: "it is always advisable to carry a gift with you each time you",
     bemba: "Ciisuma ukusenda ubupe nga mulefwaya ukumonana n..."
